8c7ce62c7975b39b981aeea501390aeb6944ec22
zoutepopcorn <zoutepopcorn@hotmail.com>
2017-05-11 16:23:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 apt-get install fbi  -y
 wget https://img.gta5-mods.com/q75/images/dutch-police-stations-oiv/9e7183-GTA5%202016-03-11%2019-22-07-66.png -O /etc/splash.png
 cp asplash /etc/init.d/asplashscreen
-sudo chmod a+x /etc/init.d/asplashscreen
-sudo insserv /etc/init.d/asplashscreen
+chmod a+x /etc/init.d/asplashscreen
+insserv /etc/init.d/asplashscreen
 reboot
