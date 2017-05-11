3c74367e94c27d8ee68c517951ed280dd050bba0
zoutepopcorn <zoutepopcorn@hotmail.com>
2017-05-11 16:47:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 apt-get update
 apt-get install fbi  -y
+cp splashscreen.service /etc/systemd/system/splashscreen.service
 wget https://img.gta5-mods.com/q75/images/dutch-police-stations-oiv/9e7183-GTA5%202016-03-11%2019-22-07-66.png -O splash.png
 fbi -a splash.png
 reboot
